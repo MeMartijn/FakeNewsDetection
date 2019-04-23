81f12972097062beccf213a878c9b19c21c7adbc
Martijn <martijn-1998@hotmail.com>
2019-04-23 09:02:02 +0200

    chore: added comments, changed dir

--- code/data_loader.py
+++ code/data_loader.py
@@ -4,17 +4,22 @@ import pandas as pd
 class DataLoader:
     '''A class which holds functionality to load and interact with the data from the research article'''
     def __init__(self):
+        # Location of the data directory
+        self.data_dir = 'data'
+
+        # Paths of the main dataset files
         self.data_link = self.get_data_loc()
+
+        # Dataframes of the train, test and validation datasets
         self.df = self.get_dfs()
     
     def get_data_loc(self):
         '''Returns the file path for the data'''
-        data_dir = 'Data'
         dir_name = 'liar_dataset'
         
         def ensemble_path(file):
             '''Short function to ensemble the paths of data files'''
-            return data_dir + '/' + dir_name + '/' + file
+            return self.data_dir + '/' + dir_name + '/' + file
     
         if dir_name in os.listdir('Data'):
             return {
@@ -31,3 +36,7 @@ class DataLoader:
             dataset: pd.read_csv(self.data_link[dataset], sep='\t', header=None, index_col=0, names=['id', 'label', 'statement', 'subjects', 'speaker', 'speaker_job', 'state', 'party', 'barely_true_count', 'false_count', 'half_true_count', 'mostly_true_count', 'pants_on_fire_count', 'context'])
             for dataset in self.data_link.keys()
         }
+
+    def get_bow(self):
+        '''Returns bag of words representation of the dataset'''
+        return 'To be constructed'
