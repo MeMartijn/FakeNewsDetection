d88a46fd1126ec04ba0ee0badff39c554d3f978d
Martijn <martijn-1998@hotmail.com>
2019-05-01 13:36:14 +0200

    chore: created infersent embeddings

--- code/data_loader.py
+++ code/data_loader.py
@@ -1,9 +1,17 @@
 import os
 import pandas as pd
+import numpy as np
+
+# Bag of Words imports
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import string
 from sklearn.feature_extraction.text import CountVectorizer
+
+# InferSent imports
+import nltk
+import torch
+from models import InferSent
 
 class DataLoader:
     '''A class which holds functionality to load and interact with the data from the research article'''
@@ -113,6 +121,87 @@ class DataLoader:
                 for dataset in dfs.keys():
                     dfs[dataset].to_pickle(os.path.join(self.data_dir, bow_dir, dataset + '.pkl'))
                 print('Saved the datasets at ' + bow_dir)
+
+                return dfs
+
+        return init()
+
+    def get_infersent(self):
+        '''Returns InferSent representation of the dataset'''
+        # Directory name for saving the datasets
+        infersent_dir = 'infersent'
+        fasttext_dir = 'fast-text'
+        encoder_dir = 'encoder'
+
+        def check_requirements():
+            '''For training InferSent embeddings, a few data files must be present. This function checks these requirements. Returns either nothing or errors.'''
+            # Make sure this is up to date
+            nltk.download('punkt')
+            
+            if not fasttext_dir in os.listdir(self.data_dir):
+                print('Please refer to the InferSent installation instructions over here: https://github.com/facebookresearch/InferSent')
+                raise ValueError('Please download the fasttext vector file from https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M-subword.zip before continuing, and place the vector file (crawl-300d-2M.vec) in data/fast-text/.')
+            
+            if not encoder_dir in os.listdir():
+                print('Please refer to the InferSent installation instructions over here: https://github.com/facebookresearch/InferSent')
+                raise ValueError('Please run the following command and rerun this function: !curl -Lo encoder/infersent2.pickle https://dl.fbaipublicfiles.com/senteval/infersent/infersent2.pkl')
+
+        def create_dataframe(df, encoder):
+            '''Create an InferSent dataframe from another dataframe'''
+            # Create a dataframe from the transformed statements
+            new_df = pd.DataFrame(encoder.encode(df.statement, tokenize = True))
+
+            # Add referencing columns
+            new_df['label'] = list(df.label)
+            new_df['id'] = list(df.index)
+            new_df.set_index('id', inplace=True)
+
+            return new_df
+
+        def init():
+            '''Initialize all logic from the main function'''
+            # Check whether there is a file containing the InferSent data already present
+            if infersent_dir in os.listdir(self.data_dir):
+                return {
+                    dataset: pd.read_pickle(os.path.join(self.data_dir, infersent_dir, dataset + '.pkl'))
+                    for dataset in self.df.keys()
+                }
+            else:
+                print('Creating InferSent representation and saving them as files...')
+
+                # Check whether model requirements are met
+                check_requirements()
+
+                # Create a storage directory
+                os.mkdir(self.data_dir + '/' + infersent_dir)
+
+                # Load pre-trained model
+                model_version = 2
+                MODEL_PATH = "encoder/infersent%s.pkl" % model_version
+                params_model = {'bsize': 64, 'word_emb_dim': 300, 'enc_lstm_dim': 2048, 'pool_type': 'max', 'dpout_model': 0.0, 'version': model_version}
+                model = InferSent(params_model)
+                model.load_state_dict(torch.load(MODEL_PATH))
+
+                # Keep it on CPU or put it on GPU
+                use_cuda = False
+                model = model.cuda() if use_cuda else model
+
+                # Set word vectors
+                W2V_PATH = os.path.join(self.data_dir, fasttext_dir, 'crawl-300d-2M.vec')
+                model.set_w2v_path(W2V_PATH)
+
+                # Build vocabulary
+                model.build_vocab(self.all_statements, tokenize = True)
+
+                dfs = {
+                    dataset: create_dataframe(self.df[dataset], model)
+                    for dataset in self.df.keys()
+                }
+
+                # Save the datasets as pickle files
+                for dataset in dfs.keys():
+                    dfs[dataset].to_pickle(os.path.join(self.data_dir, infersent_dir, dataset + '.pkl'))
+                print('Saved the datasets at ' + infersent_dir)
 
                 return dfs
 
