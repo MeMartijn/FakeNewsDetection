462143596d97cd3899657e84927a9f1efc6cc2ac
Martijn <martijn-1998@hotmail.com>
2019-06-17 20:35:53 +0200

    chore: added penalty param

--- code/classifiers.py
+++ code/classifiers.py
@@ -102,17 +102,17 @@ class Classifiers:
         return acc
 
     @staticmethod
-    def get_svm_score(X_train, X_test, X_validation, y_train, y_test, y_validation):
+    def get_svm_score(X_train, X_test, X_validation, y_train, y_test, y_validation, penalty = 'l2'):
         param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
-        gs = GridSearch(model = LogisticRegression(), param_grid = param_grid)
+        gs = GridSearch(model = LogisticRegression(penalty = penalty), param_grid = param_grid)
         gs.fit(X_train, y_train, X_validation, y_validation)
 
         return gs.score(X_test, y_test)
 
     @staticmethod
-    def get_logres_score(X_train, X_test, X_validation, y_train, y_test, y_validation):
+    def get_logres_score(X_train, X_test, X_validation, y_train, y_test, y_validation, penalty = 'l2'):
         param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
-        gs = GridSearch(model = LogisticRegression(), param_grid = param_grid)
+        gs = GridSearch(model = LogisticRegression(penalty = penalty), param_grid = param_grid)
         gs.fit(X_train, y_train, X_validation, y_validation)
 
         return gs.score(X_test, y_test)
