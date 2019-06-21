48c8f7d7c8922e8a1351eb37d830772e1ff011ac
Martijn <martijn-1998@hotmail.com>
2019-06-21 23:40:52 +0200

    fix: corrected classifier

--- code/classifiers.py
+++ code/classifiers.py
@@ -146,7 +146,7 @@ class Classifiers:
     @staticmethod
     def get_svm_score(X_train, X_test, X_validation, y_train, y_test, y_validation, penalty = 'l2'):
         param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
-        gs = GridSearch(model = LogisticRegression(penalty = penalty), param_grid = param_grid)
+        gs = GridSearch(model = LinearSVC(penalty = penalty), param_grid = param_grid)
         gs.fit(X_train, y_train, X_validation, y_validation)
 
         return gs.score(X_test, y_test)
